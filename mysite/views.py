c316c46aab91cc82665899421cecc43caecab4bb
11056002 <11056002@ntub.edu.tw>
2023-10-18 15:25:39 +0800

    add two new templates

--- mysite/views.py
+++ mysite/views.py
@@ -2,12 +2,21 @@ from django.shortcuts import render
 from mysite.models import Post
 from django.http import HttpResponse
 from datetime import datetime
+from django.shortcuts import redirect
+
 def homepage(request):
     posts = Post.objects.all()
     now = datetime.now()
     return render(request, 'index.html', locals())
 def showpost(request, slug):
-    post = Post.objects.get(slug = slug) 
+    try:  
+        post = Post.objects.get(slug = slug) 
+        if post != None:
+            return render(request, 'post.html',locals())
+        else:
+            return redirect("/")
+    except:
+        return redirect("/")
     #select* from post where slug = %slug
     return render(request, 'post.html', locals())
 
