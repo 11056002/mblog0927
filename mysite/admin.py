282095a58512126dc1eaac453dd499c2b422f8ee
11056002 <11056002@ntub.edu.tw>
2023-11-29 16:16:13 +0800

    add comments

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from mysite.models import Post
+from mysite.models import Post, Product, Comments
 # Register your models here.
+
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title','slug','pub_date')
     
-admin.site.register(Post,PostAdmin)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('text','pub_date')
+    
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comments, CommentAdmin)
